e775b1a0ab4c4d63432b1be8cead056c39b9886a
Aaron T <troskaaaron@gmail.com>
2020-04-18 13:57:30 -0500

    blockchain working

--- blockchain/miner.py
+++ blockchain/miner.py
@@ -9,6 +9,41 @@ from timeit import default_timer as timer
 
 import random
 
+# def proof_of_work(block):
+#     """
+#     Simple Proof of Work Algorithm
+#     Stringify the block and look for a proof.
+#     Loop through possibilities, checking each one against `valid_proof`
+#     in an effort to find a number that is a valid proof
+#     :return: A valid proof for the provided block
+#     """
+#     block_string = json.dumps(block, sort_keys=True)
+#     proof = 0
+
+#     while valid_proof(block_string, proof) is False:
+#         proof += 1
+
+#     # return proof
+#     return proof
+
+
+# def valid_proof(block_string, proof):
+#     """
+#     Validates the Proof:  Does hash(block_string, proof) contain 6
+#     leading zeroes?  Return true if the proof is valid
+#     :param block_string: <string> The stringified block to use to
+#     check in combination with `proof`
+#     :param proof: <int?> The value that when combined with the
+#     stringified previous block results in a hash that has the
+#     correct number of leading zeroes.
+#     :return: True if the resulting hash is a valid proof, False otherwise
+#     """
+#     guess = block_string + str(proof) 
+#     guess = guess.encode()
+    
+#     hash_value = hashlib.sha256(guess).hexdigest()
+    
+#     return hash_value[:3] == '000'
 
 def proof_of_work(last_proof):
     """
@@ -24,7 +59,15 @@ def proof_of_work(last_proof):
 
     print("Searching for next proof")
     proof = 0
-    #  TODO: Your code here
+
+    last_proof
+    last_hash = str(last_proof) 
+    last_hash = last_hash.encode()
+    last_hash_value = hashlib.sha256(last_hash).hexdigest()
+
+
+    while valid_proof(last_hash_value, proof) is False:
+        proof += 1
 
     print("Proof found: " + str(proof) + " in " + str(timer() - start))
     return proof
@@ -38,6 +81,14 @@ def valid_proof(last_hash, proof):
 
     IE:  last_hash: ...AE912345, new hash 12345E88...
     """
+
+    guess = str(proof) 
+    guess = guess.encode()
+    
+    hash_value = hashlib.sha256(guess).hexdigest()
+    
+    return last_hash[-5:] == hash_value[:5]
+
 
     # TODO: Your code here!
     pass
